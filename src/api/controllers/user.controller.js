563576c6ce1e498f6d5ec913b856e3d7cbc1b81b
quanghiep03198 <quanghiep03198@gmail.com>
2023-09-20 15:38:39 +0700

    update

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,16 +1,19 @@
+import createHttpError from 'http-errors';
 import _configs from '../../configs/app.config';
 import HttpStatusCode from '../../constants/httpStatus';
 import { HttpResponse, AsyncFn } from '../../helpers/http';
 import userService from '../services/user.service';
 import jwt from 'jsonwebtoken';
+import UserModel from '../models/user.model';
 
 export default {
 	/**
-	 *  @method post('signin')
+	 * @endpoint /signin
+	 * @method Post()
 	 * */
 	signin: AsyncFn(async (req, res) => {
 		const user = await userService.signin(req.body);
-		const accessToken = jwt.sign({ email: user.email, role: user.role }, _configs.JWT_SECRET, {
+		const accessToken = jwt.sign({ id: user._id, email: user.email, role: user.role }, _configs.JWT_SECRET, {
 			expiresIn: _configs.JWT_EXPIRES
 		});
 		const response = new HttpResponse({ user, accessToken }, 'Signed in successfully');
@@ -18,21 +21,46 @@ export default {
 	}),
 
 	/**
-	 *  @method post('/create-user')
+	 * @endpoint /create-user
+	 * @method Post()
 	 * */
 	createUser: AsyncFn(async (req, res) => {
 		const newUser = await userService.createUser(req.body);
 		const response = new HttpResponse(newUser, 'Created new user');
 		return res.status(HttpStatusCode.CREATED).json(response);
 	}),
-
 	/**
-	 *  @method get('/users')
+	 * @endpoint /users/info
+	 * @method Get()
+	 */
+	getUserInfo: AsyncFn(async (req, res) => {
+		const currentUser = await UserModel.findById(req.auth);
+		const response = new HttpResponse(currentUser);
+		return res.status(HttpStatusCode.OK).json(response);
+	}),
+	/**
+	 * @endpoint /users
+	 * @method Get()
 	 * */
 	getUsers: AsyncFn(async (req, res) => {
 		const limit = (req.query.limit ??= 10);
 		const users = await userService.getAllUsers(limit);
 		const response = new HttpResponse(users, 'Ok');
+		return res.status(HttpStatusCode.OK).json(response);
+	}),
+	/**
+	 * @endpoint /refresh-token
+	 * @method Get()
+	 */
+	refreshToken: AsyncFn(async (req, res) => {
+		const id = req.params.authId;
+		if (!id) throw createHttpError.BadRequest('Failed to get refresh token due to missing auth ID');
+		const user = await UserModel.findById(id);
+		if (!user) throw createHttpError.NotFound('Failed to get refresh token due to unable to find user');
+		const refreshToken = jwt.sign({ id: user._id, email: user.email, role: user.role }, _configs.JWT_SECRET, {
+			expiresIn: _configs.JWT_EXPIRES
+		});
+		const response = new HttpResponse(refreshToken, 'Get refresh token successfully');
 		return res.status(HttpStatusCode.OK).json(response);
 	})
 };
--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,13 +1,18 @@
 import { isHttpError } from 'http-errors';
 import HttpStatusCode from '../constants/httpStatus';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 class HttpException extends Error {
 	message;
 	status;
 	constructor(error) {
-		super(error)
+		super(error);
 		this.message = error.message;
-		this.status = isHttpError(error) ? error.status : HttpStatusCode.INTERNAL_SERVER_ERROR;
+		this.status = isHttpError(error)
+			? error.status
+			: error instanceof JsonWebTokenError
+			? HttpStatusCode.UNAUTHORIZED
+			: HttpStatusCode.INTERNAL_SERVER_ERROR;
 	}
 }
 
@@ -15,10 +20,10 @@ class HttpResponse {
 	message;
 	data;
 	/**
-	 * @param {unknown} data 
-	 * @param {string} message 
+	 * @param {unknown} data
+	 * @param {string} message
 	 */
-	constructor (data, message) {
+	constructor(data, message) {
 		this.data = data;
 		this.message = message;
 	}
@@ -28,14 +33,10 @@ class HttpResponse {
  * @param {(req: import("express").Request, res:import("express").Response, next: import('express').NextFunction) =>  Promise<any>} fn
  * @return {(req: import("express").Request, res:import("express").Response, next: import('express').NextFunction)}
  */
-const AsyncFn = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(error => {
-	const httpException = new HttpException(error)
-	return res.status(httpException.status).json(httpException)
-}
-)
+const AsyncFn = (fn) => (req, res, next) =>
+	Promise.resolve(fn(req, res, next)).catch((error) => {
+		const httpException = new HttpException(error);
+		return res.status(httpException.status).json(httpException);
+	});
 
-
-
-
-export { AsyncFn  , HttpException, HttpResponse };
-
+export { AsyncFn, HttpException, HttpResponse };
